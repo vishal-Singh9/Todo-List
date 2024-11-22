1814e1a7644dba893a3c0f6f38be54d1392d0054
Vishal Singh <vishalsng9@gmail.com>
2024-11-22 12:00:12 +0530

    todo list

---
+++ src/context/TodoContext.js
@@ -0,0 +1,33 @@
+import { useContext,createContext } from 'react'
+
+
+export const TodoContext = createContext({
+    todos:[{
+        id:1,
+        todo:"todo msg",
+        completed:false
+
+    }],
+    addTodo :(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deletedTodo:(id)=>{},
+    toggleComplete:(id)=>{}
+});
+
+
+
+export const useTodo = ()=>{
+    return useContext(TodoContext);
+}
+
+
+export const TodoProvider = TodoContext.Provider
+
+
+
+
+
+
+
+
+
